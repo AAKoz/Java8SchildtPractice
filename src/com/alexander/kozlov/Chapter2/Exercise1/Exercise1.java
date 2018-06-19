3d1df80fc168d69e7e7d52d6fc241e8668284293
AleksanderKozlov <shurasik9509@rambler.ru>
2018-06-19 16:31:26 +0300

    Java8SchidldtPractice - prepared IDE and exercise1

---
+++ src/com/alexander/kozlov/Chapter2/Exercise1/Exercise1.java
@@ -0,0 +1,12 @@
+package com.alexander.kozlov.Chapter2.Exercise1;
+
+/*
+    Это простая программа на Java.
+    Присвоить исходному файлу имя "Exercise1.java"
+ */
+public class Exercise1 {
+    // Эта программа начинается с вызова метода main()
+    public static void main(String[] args) {
+        System.out.println("Простая программа на Java.");
+    }
+}

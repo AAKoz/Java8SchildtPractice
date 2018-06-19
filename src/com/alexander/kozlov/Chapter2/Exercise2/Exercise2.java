1f1f3464848d7e05de81d756ce663244cf1af66e
AleksanderKozlov <shurasik9509@rambler.ru>
2018-06-19 17:15:05 +0300

    Java8SchidldtPractice - added Exercise2

---
+++ src/com/alexander/kozlov/Chapter2/Exercise2/Exercise2.java
@@ -0,0 +1,18 @@
+package com.alexander.kozlov.Chapter2.Exercise2;
+
+/*
+Это еще один короткий пример программы.
+Присвоить исходному файлу имя "Exercise2.java"
+*/
+
+public class Exercise2 {
+
+    public static void main(String args[]) {
+        int num;
+        num = 100;
+        System.out.println("Этo переменная num: " + num);
+        num = num * 2;
+        System.out.print("Знaчeниe переменной num * 2 равно ");
+        System.out.println(num);
+    }
+}
